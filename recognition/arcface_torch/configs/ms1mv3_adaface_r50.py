175b33989206fd12d9d0afc0f89c7d9c57f3f3c0
wuxuedaifu <wuxuedaifu@gmail.com>
2026-06-27 10:00:03 +0400

    feat(adaface): add training script and MS1MV3 configs
    train_adaface.py mirrors train_v2.py but uses norm_output=True backbone,
    AdaFaceLoss, and PartialFC_V2_AdaFace. Configs for R50 and R100 add
    adaface_m/h/s/t_alpha hyperparameters on top of the ms1mv3 base settings.

---
+++ recognition/arcface_torch/configs/ms1mv3_adaface_r50.py
@@ -0,0 +1,28 @@
+from easydict import EasyDict as edict
+
+config = edict()
+config.network = "r50"
+config.resume = False
+config.output = None
+config.embedding_size = 512
+config.sample_rate = 1.0
+config.fp16 = True
+config.momentum = 0.9
+config.weight_decay = 5e-4
+config.batch_size = 128
+config.lr = 0.1
+config.verbose = 2000
+config.dali = False
+
+config.rec = "/train_tmp/ms1m-retinaface-t1"
+config.num_classes = 93431
+config.num_image = 5179510
+config.num_epoch = 20
+config.warmup_epoch = 0
+config.val_targets = ['lfw', 'cfp_fp', "agedb_30"]
+
+# AdaFace hyperparameters
+config.adaface_m = 0.4
+config.adaface_h = 0.333
+config.adaface_s = 64.0
+config.adaface_t_alpha = 0.01
